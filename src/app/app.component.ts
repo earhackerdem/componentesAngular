ad22f3e4fd2ce41c520b08372ce323eab2a4f67a
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-09-22 12:09:37 -0500

    Componente para producto - 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   imgParent = '';
+  products: Product[] = [
+    { id: '1',
+    name: 'Product 1',
+    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
+    price: 100},
+    { id: '2',
+    name: 'Product 1',
+    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
+    price: 100},
+    { id: '3',
+    name: 'Product 1',
+    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
+    price: 100}
+  ];
 
   onLoaded(img: string){
     console.log("log padre",img);
