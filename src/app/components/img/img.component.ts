523c7906ac7d94b3af9dca0784b2493607176d74
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-09-22 15:19:11 -0500

    Ciclo de vida de componentes - 6

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter, OnChanges, SimpleChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit,OnDestroy {
   @Input() img: string = "";
   @Output() loaded = new EventEmitter<string>();
   imageDefault = 'https://www.m2crowd.com/core/i/placeholder.png';
-  constructor() { }
+  constructor() {
+    //before render
+    //No async -- once time
+    console.log("constructor",'imgValue =>',this.img);
+   }
+
+   ngOnChanges(changes: SimpleChanges): void {
+   //before render
+   //changes inputs -- nTimes
+   console.log("ngOnChanges",'imgValue=>',this.img)
+   }
 
   ngOnInit(): void {
+    //before render
+    //async - fetch -- one time
+    console.log("ngOnInit",'imgValue =>',this.img);
+  }
+
+  ngAfterViewInit(): void {
+      //after render
+      //children handler
+      console.log("ngAfterViewInit",'imgValue =>',this.img)
+  }
+
+  ngOnDestroy(){
+    console.log("ngOnDestroy",'imgValue =>',this.img)
   }
 
   imgError(){
