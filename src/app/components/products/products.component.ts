fa13d18943cf3c21950f94315d625bbf460f53cd
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-09-23 13:39:17 -0500

    Obteniendo datos de una API

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
 import { StoreService } from 'src/app/services/store.service';
+import { ProductsService } from 'src/app/services/products.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -8,44 +9,23 @@ import { StoreService } from 'src/app/services/store.service';
 })
 export class ProductsComponent implements OnInit {
 
-  myShoppingCart:Product[] = [];
-  total:number = 0;
+  myShoppingCart: Product[] = [];
+  total: number = 0;
+  products: Product[] = [];
 
-  products: Product[] = [
-    { id: '1',
-    name: 'Product 1',
-    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
-    price: 100},
-    { id: '2',
-    name: 'Product 1',
-    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
-    price: 100},
-    { id: '3',
-    name: 'Product 1',
-    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
-    price: 100},
-    { id: '3',
-    name: 'Product 1',
-    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
-    price: 100},
-    { id: '3',
-    name: 'Product 1',
-    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
-    price: 100},
-    { id: '3',
-    name: 'Product 1',
-    image: 'https://web-static.wrike.com/cdn-cgi/image/width=880,format=auto,q=80/blog/content/uploads/2015/12/errores-fatales-en-el-lanzamiento-de-un-producto.jpg',
-    price: 100}
-  ];
 
-  constructor(private storeService: StoreService) {
+  constructor(private storeService: StoreService, private productService: ProductsService) {
     this.myShoppingCart = this.storeService.getShoppingCart();
   }
 
   ngOnInit(): void {
+    this.productService.getAllProducts()
+      .subscribe(data => {
+       this.products = data;
+      })
   }
 
-  onAddToShoppingCart(product:Product){
+  onAddToShoppingCart(product: Product) {
     this.storeService.addProduct(product);
     this.total = this.storeService.getTotal();
   }
