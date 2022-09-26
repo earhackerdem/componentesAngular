d6336dba5d824ee2f99dd660c0b3fe0994629f8b
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-09-26 18:21:55 -0500

    Solicitudes DELETE - 7

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -112,5 +112,16 @@ export class ProductsComponent implements OnInit {
     })
   }
 
+  deleteProduct(){
+    const id = this.productChosen.id;
+    this.productService.delete(id)
+    .subscribe(() => {
+      const productIndex = this.products.findIndex( item => item.id === this.productChosen.id);
+      this.products.splice(productIndex,1);
+      this.showProductDetail = false;
+
+    });
+  }
+
 
 }
