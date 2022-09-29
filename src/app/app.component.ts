9c8661e8d9009443797240e52de1f048cf0d7697
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-09-29 15:18:54 -0500

    Login y manejo de Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model';
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +11,34 @@ export class AppComponent {
   imgParent = '';
   showImage = true;
 
+  constructor(private authService: AuthService, private userService: UsersService){}
 
   onLoaded(img: string){
   }
 
   toggleImg(){
     this.showImage = !this.showImage;
+  }
+
+  createUser(){
+    this.userService.create({
+      name: "Earvin",
+      email: "earvin@mail.com",
+      password: "password"
+    })
+    .subscribe( rta => {
+      console.log(rta)
+    });
+  }
+
+  login(){
+    this.authService.login('earvin@mail.com','password')
+    .subscribe( rta => {
+      console.log(rta.access_token)
+    });
+  }
+
+  profile(){
+
   }
 }
