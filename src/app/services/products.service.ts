11eb50d4612b8ca54cd111ae1753809995e1616c
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2022-09-29 12:33:37 -0500

    Transformar peticiones - 14

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
 import { Product, UpdateProductDTO } from '../models/product.model';
 import { CreateProductDTO } from '../models/product.model';
-import { catchError, retry } from 'rxjs/operators';
+import { catchError, retry, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { throwError } from 'rxjs';
 @Injectable({
@@ -22,7 +22,16 @@ export class ProductsService {
       params = params.set('limit',limit);
       params = params.set('offset',offset);
     }
-    return this.http.get<Product[]>(this.apiUrl,{ params });
+    return this.http.get<Product[]>(this.apiUrl,{ params })
+    .pipe(
+      retry(3),
+      map(products => products.map(item => {
+        return {
+          ...item,
+          taxes: .19 * item.price
+        }
+      }))
+    );
   }
 
   getProduct(id: string) {
@@ -47,7 +56,13 @@ export class ProductsService {
     return this.http.get<Product[]>(`${this.apiUrl}`,{
       params:{ limit, offset }
     }).pipe(
-      retry(3)
+      retry(3),
+      map(products => products.map(item => {
+        return {
+          ...item,
+          taxes: .19 * item.price
+        }
+      }))
     );
   }
 
